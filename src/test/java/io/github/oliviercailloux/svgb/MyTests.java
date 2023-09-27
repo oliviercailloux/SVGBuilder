19e1cbc925c77234e0f0facc36372c171906505b
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-09-27 17:01:57 +0200

    test

--- src/test/java/io/github/oliviercailloux/svgb/MyTests.java
+++ src/test/java/io/github/oliviercailloux/svgb/MyTests.java
@@ -1,10 +1,21 @@
 package io.github.oliviercailloux.svgb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.google.common.io.Resources;
+
+import io.github.oliviercailloux.jaris.xml.DomHelper;
 
 public class MyTests {
 	@SuppressWarnings("unused")
@@ -13,6 +24,15 @@ public class MyTests {
 	@Test
 	void testSomething() throws Exception {
 		LOGGER.info("Started tests.");
-		assertTrue(false);
+		DomHelper d = DomHelper.domHelper();
+		Document doc = d.svg();
+		SvgHelper h = SvgHelper.using(doc);
+		Element e = h.ellipse(SvgPoint.zero(), SvgSize.square(100));
+		doc.getDocumentElement().appendChild(e);
+		
+		String actual = d.toString(doc);
+		// Files.writeString(Path.of("out.svg"), actual);
+		String expected = Resources.toString(MyTests.class.getResource("Ellipse.svg"), StandardCharsets.UTF_8);
+		assertEquals(expected, actual);
 	}
 }
